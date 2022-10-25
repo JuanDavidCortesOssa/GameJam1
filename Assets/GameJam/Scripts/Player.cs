69cd12027debf5dc7486aa638a49a9a1a2367d34
JuanDavidCortesOssa <64558296+JuanDavidCortesOssa@users.noreply.github.com>
2022-10-25 10:20:18 -0500

    Basic 2D Player Movement

---
+++ Assets/GameJam/Scripts/Player.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float horizontal;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpingPower;
+    private bool isFacingRight;
+
+
+    [SerializeField] private Rigidbody2D rigidBody;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeVariables();
+    }
+
+    private void InitializeVariables()
+    {
+        isFacingRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rigidBody.velocity.y > 0f)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y * 0.5f);
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void MovePlayer()
+    {
+        rigidBody.velocity = new Vector2(horizontal * speed, rigidBody.velocity.y);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+
+}
